4ff4d98eabc13ab1b39c6b94c91612ba11b09429
Alby-Sus <kamisatoayato205@gmail.com>
2025-03-06 13:33:03 +0700

    Làm lại BG

---
+++ Assets/Script/Button.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void NextScene()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Menu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Level 0");
+    }
+    public void ExitGame()
+    {
+        Debug.Log("Đa thoat game");
+        Application.Quit();
+    }
+}
